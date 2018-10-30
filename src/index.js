664dbd2c44325f983058e612faaa2214f3f94308
Adam Young <adamyoung012@gmail.com>
2018-10-30 14:07:30 +1100

    Issue #1: Load and serve tiles for two different regions.

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ const MBTiles = require('@mapbox/mbtiles').registerProtocols(tilelive);
 const fs = require('fs');
 const download = require('download');
 var cors = require('cors')
-import {getAllTiles, getList, loadTilesDb} from './tilesaggr'
+import {loadAllTiles, getTile} from './tilesaggr'
 
 function startServer(port) {
   return new Promise(function(resolve, reject){
@@ -15,32 +15,14 @@ function startServer(port) {
   });
 }
 
-function getTile(source, z, x, y) {
-  return new Promise(function(resolve, reject){
-    source.getTile(z, x, y, function(err, tile, headers) {
-        if (err) {
-          reject(err);
-        } else {
-          resolve([headers, tile]);
-        }
-    });
-  });
-}
-
 async function main3() {
   var tiledict = {};
   tiledict["mount_alexander_shire_network"] = "https://cloudstor.aarnet.edu.au/plus/s/oh23zw4a0Vy4PNQ/download";
   tiledict["surf_coast_shire_network"] = "https://cloudstor.aarnet.edu.au/plus/s/JK7STxWGKI2jNe4/download";
 
-  // Download all tiles from cloud storage if necessary
-  getAllTiles(tiledict)
+  // Download all tiles from cloud storage if necessary, and load them into memory
+  loadAllTiles(tiledict);
 
-  // List the MBTiles files in this directory
-  const list = await getList();
-
-  // Load the one we want (assumes it is there)
-  console.log("Loading %s", "mount_alexander_shire_network");
-  const tiles = await loadTilesDb(list["mount_alexander_shire_network"]).catch(error => console.log(error));
   // Start the express server
   const port = 12345;
   console.log("Starting the server on local port %d", port);
@@ -64,7 +46,7 @@ async function main3() {
     const y = parseInt(req.params.y);
     const z = parseInt(req.params.z);
     console.log("zxy[%d,%d,%d] ", z,x,y);
-    getTile(tiles, z, x, y).then(function(data) {
+    getTile(req.params.layer, z, x, y).then(function(data) {
       const headers = data[0];
       const img = data[1];
       console.log(headers);
--- src/tilesaggr.js
+++ src/tilesaggr.js
@@ -7,8 +7,12 @@ var cors = require('cors')
 
 var dir = ".";
 
-export async function getAllTiles(tiles) {
+// where we will store all tile sources. Will load every mbtiles region for now. Will memory be an issue?
+var tilesources = {};
+
+export async function loadAllTiles(tiles) {
   for (var key in tiles) {
+    // Download the mbtiles ready for loading
     var mbtilesFile = key+".mbtiles";
     var mbtilesUrl = tiles[key];
     if (!fs.existsSync(mbtilesFile)) {
@@ -17,6 +21,15 @@ export async function getAllTiles(tiles) {
     } else {
       console.log("Found %s so will use it",mbtilesFile);
     }
+  }
+
+  // We have downloaded all regions
+  const mbtilesList = await getList();
+
+  // load them into our tilesources datastructure
+  for (var mbtilesKey in mbtilesList) {
+    console.log("Loading %s", mbtilesKey);
+    tilesources[mbtilesKey] = await loadTilesDb(mbtilesList[mbtilesKey]);
   }
 }
 
@@ -37,7 +50,10 @@ function getTilesDb(url, dest) {
   });
 }
 
-export function loadTilesDb(url) {
+/**
+ * Load tiles into memory
+ */
+function loadTilesDb(url) {
   return new Promise(function(resolve, reject){
     tilelive.load(url, function(err, source) {
       if (err) {
@@ -49,19 +65,26 @@ export function loadTilesDb(url) {
   });
 }
 
-function getTile(source, z, x, y) {
+export function getTile(region, z, x, y) {
   return new Promise(function(resolve, reject){
-    source.getTile(z, x, y, function(err, tile, headers) {
+    // check the source string is a valid one
+    if (tilesources.hasOwnProperty(region)) {
+      // grab the tile from this region
+      var source = tilesources[region];
+      source.getTile(z, x, y, function(err, tile, headers) {
         if (err) {
           reject(err);
         } else {
           resolve([headers, tile]);
         }
-    });
+      });
+    } else { // region is invalid or hasn't been downloaded
+      reject("Provided region is invalid.")
+    }
   });
 }
 
-export function getList() {
+function getList() {
   return new Promise(function(resolve, reject){
     tilelive.list(dir, function(err, data){
       if (err) {
