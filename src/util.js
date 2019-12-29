d88bf368c376ba749e8da438654af82d9688ff3b
Adam Young <adamyoung012@gmail.com>
2019-12-30 08:03:00 +1100

    Revert "output pre-process and serve" - crashes production server due to too much memory use while extracting events xml
    This reverts commit db7196211a6324c99bcd1dc9c95129cc3f7650f1.

--- src/util.js
+++ src/util.js
@@ -1,5 +1,4 @@
 const fs = require('fs');
-const path = require('path');
 const download = require('download');
 
 const reproj = require('reproject');
@@ -20,32 +19,6 @@ proj4.defs([
   ]
 ]);
 
-export function searchRecursive(dir, pattern) {
-  // This is where we store pattern matches of all files inside the directory
-  var results = [];
-
-  // Read contents of directory
-  fs.readdirSync(dir).forEach(function (dirInner) {
-    // Obtain absolute path
-    dirInner = path.resolve(dir, dirInner);
-
-    // Get stats to determine if path is a directory or a file
-    var stat = fs.statSync(dirInner);
-
-    // If path is a directory, scan it and combine results
-    if (stat.isDirectory()) {
-      results = results.concat(searchRecursive(dirInner, pattern));
-    }
-
-    // If path is a file and ends with pattern then push it onto results
-    if (stat.isFile() && dirInner.endsWith(pattern)) {
-      results.push(dirInner);
-    }
-  });
-
-  return results;
-};
-
 /**
  * download a given resource to a destination folder
  */
@@ -61,138 +34,6 @@ export function downloadResource(url, dir, dest) {
       }
     });
   });
-}
-
-export function parseOutputEventsXml(eventsfile) {
-  return new Promise((resolve, reject) => {
-
-    try {
-      console.log("Attempting to parse events XML...");
-      fs.readFile(eventsfile, function (zerror, zbuf) {
-        if (zerror) reject(zerror);
-        zlib.gunzip(zbuf, function (error, buf) {
-          if (error) reject(error);
-
-          // access data as buffer
-          var jsonObj = XML.parse(buf.toString());
-          console.log("XML Events parsed");
-
-          resolve(jsonObj);
-        });
-      });
-    } catch(error) {
-      reject(err);
-    }
-  });
-}
-
-export function parseOutputNetworkXml(networkfile) {
-  return new Promise((resolve, reject) => {
-
-    try {
-      console.log("Attempting to parse network XML...");
-      fs.readFile(networkfile, function (zerror, zbuf) {
-        if (zerror) reject(zerror);
-        zlib.gunzip(zbuf, function (error, buf) {
-          if (error) reject(error);
-
-          // access data as buffer
-          var jsonObj = XML.parse(buf.toString());
-          console.log("XML network parsed");
-
-          resolve(jsonObj);
-        });
-      });
-    } catch(error) {
-      reject(err);
-    }
-  });
-}
-
-// param: array length two, events json and network json produced from output xml
-export function buildEventFrames(eventsjson, network) {
-  var nodes = {};
-  var links = {};
-
-  // attr
-  var crs;
-  if (Array.isArray(network.attributes.attribute)) {
-    for(const n of network.attributes.attribute) {
-      if (n.name == 'coordinateReferenceSystem') {
-        crs = n._Data;
-        break;
-      }
-    }
-  } else if (network.attributes.attribute.name == 'coordinateReferenceSystem') {
-    crs = network.attributes.attribute._Data
-  } else {
-    crs = 'EPSG:4326';
-  }
-  console.log("Found CRS for xml network: %s", crs);
-
-  // nodes
-  for(const n of network.nodes.node) {
-    nodes[n.id] = [n.x, n.y];
-  }
-
-  // links
-  for(const l of network.links.link) {
-    // we are filtering events on entered link, so we only care about any given
-    // link fromnode
-    links[l.id] = l.from;
-  }
-
-  // events
-  const events = eventsjson.event;
-  var frames=[];
-  var framesize=60; //seconds
-  // the vehicle must be the key, in order to track where each car goes
-  var j=0;
-  var vehicletracker = {}; // for the current frame
-  for(var i=0; i < 24 * 60 * 60 / framesize; i++) {
-    while(j<events.length && i * framesize > parseInt(events[j].time)) {
-      if (events[j].type == "entered link")
-        vehicletracker[events[j].vehicle] = events[j].link;
-      else if (events[j].type == "PersonLeavesVehicle")
-        delete vehicletracker[events[j].vehicle];
-      j++;
-    }
-
-    // we have reached the end of the timestep, build a frame
-    var f={}; // frame
-    for (const v in vehicletracker) {
-      if (vehicletracker[v] in f) {
-        f[vehicletracker[v]]++;
-      } else {
-        f[vehicletracker[v]] = 1;
-      }
-    }
-    
-    var features = [];
-    for(const l in f) { // for each link in our frame
-      const n = nodes[links[l]];
-      var x, y;
-      [x, y] = proj4(
-        crs,
-        "EPSG:4326",
-        [parseFloat(n[0]),parseFloat(n[1])]);
-      var feature = {
-        'type':'Feature',
-        'properties': {
-          'v': f[l]
-        },
-        'geometry': {
-          'type': 'Point',
-          'coordinates':[x,y]
-        }
-      };
-      features.push(feature);
-    }
-    frames.push({'type':'FeatureCollection',
-      'features':features});
-  }
-
-  return frames;
 }
 
 /**
