ab9053f112e2db10a8d5c39eea27819de51a1937
Pawan Gamage <alexandritegalle@gmail.com>
2020-03-10 09:41:33 +1100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -78,7 +78,7 @@ async function main3() {
   });
 
   server.get('/wake/please', function(req, res){
-    res.send("OKZ");
+    res.send("OKZZ");
   });
 
   // Serve the requested file (needed to get style.json)
