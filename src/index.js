d0a2b3cf766976de354e462d824b74680ae9be29
Pawan Gamage <alexandritegalle@gmail.com>
2020-03-10 10:49:32 +1100

    V2.1 testing codepipeline

--- src/index.js
+++ src/index.js
@@ -78,7 +78,7 @@ async function main3() {
   });
 
   server.get('/wake/please', function(req, res){
-    res.send("OKZ");
+    res.send("OK 2.1");
   });
 
   // Serve the requested file (needed to get style.json)
